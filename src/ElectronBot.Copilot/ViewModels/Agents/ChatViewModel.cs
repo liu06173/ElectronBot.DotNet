706bc0e7e1d5967ceef25881e24d3429793a4506
developer <developer@example.com>
2024-10-18 21:47:40 +0800

    add chat page code

--- src/ElectronBot.Copilot/ViewModels/Agents/ChatViewModel.cs
+++ src/ElectronBot.Copilot/ViewModels/Agents/ChatViewModel.cs
@@ -1,16 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using BotSharp.Abstraction.Agents;
+using BotSharp.Abstraction.Agents.Enums;
 using BotSharp.Abstraction.Agents.Models;
 using BotSharp.Abstraction.Conversations;
 using BotSharp.Abstraction.Conversations.Models;
+using BotSharp.Abstraction.Models;
 using BotSharp.Abstraction.Repositories.Filters;
+using BotSharp.Abstraction.Routing;
 using BotSharp.Abstraction.Users;
 using BotSharp.Abstraction.Users.Enums;
 using BotSharp.Abstraction.Utilities;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.DependencyInjection;
+using CommunityToolkit.Mvvm.Input;
 using ElectronBot.Braincase.Contracts.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
 
 namespace ElectronBot.Copilot.ViewModels;
 
@@ -19,11 +27,13 @@ public partial class ChatViewModel : ObservableRecipient, INavigationAware
     private readonly IConversationService _conversationService;
     private readonly IUserIdentity _userIdentity;
     private readonly IUserService _userService;
-    public ChatViewModel(IConversationService conversationService, IUserIdentity userIdentity, IUserService userService)
+    private readonly IServiceProvider _services;
+    public ChatViewModel(IConversationService conversationService, IUserIdentity userIdentity, IUserService userService, IServiceProvider services)
     {
         _conversationService = conversationService;
         _userIdentity = userIdentity;
         _userService = userService;
+        _services = services;
     }
 
     [ObservableProperty]
@@ -31,6 +41,59 @@ public partial class ChatViewModel : ObservableRecipient, INavigationAware
 
     [ObservableProperty]
     List<Conversation> _conversationList = new();
+
+    [ObservableProperty]
+    List<RoleDialogModel> _chatMessageList = new();
+
+    [ObservableProperty]
+    Conversation? _selectedConversation;
+
+    [ObservableProperty]
+    string? _sendText;
+
+    [RelayCommand]
+    public Task ConvSelectAsync(Conversation? conv)
+    {
+        if (conv == null) return Task.CompletedTask;
+        _conversationService.SetConversationId(conv.Id, new List<MessageState>());
+        var history = _conversationService.GetDialogHistory(fromBreakpoint: false);
+        ChatMessageList = history;
+        return Task.CompletedTask;
+    }
+
+    [RelayCommand]
+    public async Task SendChatAsync()
+    {
+        if (SelectedConversation == null) return;
+
+        if (string.IsNullOrEmpty(SendText)) return;
+
+        var inputMsg = new RoleDialogModel(AgentRole.User, SendText)
+        {
+            MessageId = Guid.NewGuid().ToString(),
+            CreatedAt = DateTime.UtcNow
+        };
+
+        var routing = _services.GetRequiredService<IRoutingService>();
+        routing.Context.SetMessageId(SelectedConversation.Id, inputMsg.MessageId);
+
+        _conversationService.SetConversationId(SelectedConversation.Id, new());
+
+        await _conversationService.SendMessage(SelectedConversation.AgentId, inputMsg,
+            replyMessage: null,
+            async msg =>
+            {
+                ChatMessageList.Add(msg);
+            });
+    }
+
+    [RelayCommand]
+    public Task StartChatAsync(string? sendText)
+    {
+        if (string.IsNullOrEmpty(sendText)) return Task.CompletedTask;
+        return Task.CompletedTask;
+    }
+
 
     public void OnNavigatedFrom()
     {
