83e8c04bf9fe7c5ac70b3837b577fb48c7cbcfb7
developer <developer@example.com>
2024-10-22 00:20:12 +0800

    update play image

--- src/Verdure.WinUI.Common/Players/ElectronBotPlayer.Image.cs
+++ src/Verdure.WinUI.Common/Players/ElectronBotPlayer.Image.cs
@@ -1,7 +1,66 @@
-﻿namespace Verdure.WinUI.Common.Players;
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using Verdure.ElectronBot.Core.Models;
+using Windows.Storage;
+
+namespace Verdure.WinUI.Common.Players;
 public partial class ElectronBotPlayer
 {
-    public Task PlayImageAsync(string path) => throw new NotImplementedException();
+    public async Task PlayImageAsync(string path)
+    {
+        using var image = await LoadImageAsync(path);
+
+        image.Mutate(x =>
+        {
+            x.Resize(240, 240);
+        });
+
+        // 获取转换后的数据
+        var rgbData = new byte[image.Width * image.Height * 3];
+
+        // 遍历每个像素，将Rgba32转换为Bgr24
+        for (var y = 0; y < image.Height; y++)
+        {
+            for (var x = 0; x < image.Width; x++)
+            {
+                var rgbaPixel = image[x, y];
+                var rgbIndex = (y * image.Width + x) * 3;
+                rgbData[rgbIndex] = rgbaPixel.B;
+                rgbData[rgbIndex + 1] = rgbaPixel.G;
+                rgbData[rgbIndex + 2] = rgbaPixel.R;
+            }
+        }
+
+        var frameData = new EmoticonActionFrame(rgbData, false);
+
+        _ = await _actionFrameService.SendToUsbDeviceAsync(frameData);
+    }
     public Task PlayImageAsync(Stream stream) => throw new NotImplementedException();
-    public Task PlayImageAsync(byte[] bytes) => throw new NotImplementedException();
+    public async Task PlayImageAsync(byte[] bytes)
+    {
+        var frameData = new EmoticonActionFrame(bytes, false);
+
+        _ = await _actionFrameService.SendToUsbDeviceAsync(frameData);
+    }
+
+    private async Task<Image<Rgba32>> LoadImageAsync(string imagePath)
+    {
+        if (imagePath.StartsWith("ms-appx"))
+        {
+            using var stream = await GetFileStreamAsync(imagePath);
+            return Image.Load<Rgba32>(stream);
+        }
+        else
+        {
+            return Image.Load<Rgba32>(imagePath);
+        }
+    }
+
+    private async Task<Stream> GetFileStreamAsync(string filePath)
+    {
+        var storageFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri(filePath));
+        var randomAccessStream = await storageFile.OpenAsync(FileAccessMode.Read);
+        return randomAccessStream.AsStreamForRead();
+    }
 }
